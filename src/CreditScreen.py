960aa10b72fc5898c79c5be8656a1dfd93daee63
dulanj <dulanjayasuriya@gmail.com>
2020-07-22 22:20:37 +0530

    Clean data and create model

--- src/CreditScreen.py
+++ src/CreditScreen.py
@@ -5,6 +5,7 @@ Created on 7/22/20
 '''
 import logging
 import pandas as pd
+import tensorflow as tf
 
 
 class CreditScreen():
@@ -12,11 +13,30 @@ class CreditScreen():
         # create logger
         logging.basicConfig(level=logging.DEBUG)
         self.df = self.load_data()
+        self.clean_data()
+        self.model = self.create_model()
 
     def load_data(self):
         df = pd.read_csv('../data/crx.data')
         logging.debug(df.head())
         return df
+
+    def clean_data(self):
+        self.df.dropna(inplace=True)
+
+    def create_model(self):
+        input = tf.keras.Input(shape=(15,1))
+        x = tf.keras.layers.Dense(30, activation=tf.nn.relu)(input)
+        output = tf.keras.layers.Dense(2,activation=tf.nn.softmax)(x)
+
+        model = tf.keras.Model(inputs=input, outputs=output)
+        logging.info(model.summary())
+        
+        #Compile model
+        opt = tf.keras.optimizers.SGD(lr=0.01, momentum=0.9)
+        model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
+        return model
 
     def main(self):
         pass
