b19ab359d72f817048a2fcfb54bf040788f94ea1
dulanj <dulanjayasuriya@gmail.com>
2020-07-22 21:57:30 +0530

    Initial Files

---
+++ src/CreditScreen.py
@@ -0,0 +1,18 @@
+'''
+Created on 7/22/20
+
+@author: dulanj
+'''
+
+
+class CreditScreen():
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == "__main__":
+    obj = CreditScreen()
+    obj.main()
