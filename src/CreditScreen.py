e31d3e26a5dc0224824d7f6655a9857b1df3684c
dulanj <dulanjayasuriya@gmail.com>
2020-07-22 22:04:04 +0530

    Data loading

--- src/CreditScreen.py
+++ src/CreditScreen.py
@@ -3,11 +3,20 @@ Created on 7/22/20
 
 @author: dulanj
 '''
+import logging
+import pandas as pd
 
 
 class CreditScreen():
     def __init__(self):
-        pass
+        # create logger
+        logging.basicConfig(level=logging.DEBUG)
+        self.df = self.load_data()
+
+    def load_data(self):
+        df = pd.read_csv('../data/crx.data')
+        logging.debug(df.head())
+        return df
 
     def main(self):
         pass
